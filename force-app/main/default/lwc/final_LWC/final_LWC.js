d70e06b4e2ea7469f2540d0eb7c92ddba97229ca
adi789p <71259100+adi789p@users.noreply.github.com>
2025-01-02 17:25:11 +0530

    Revert "Feature/jenkins 001"

--- force-app/main/default/lwc/final_LWC/final_LWC.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class Final_LWC extends LightningElement {}
--- force-app/main/default/lwc/jenkins001/jenkins001.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class Jenkins001 extends LightningElement {}
